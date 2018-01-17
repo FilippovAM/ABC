f01ff1a04df44c0654713732318efa24539152e2
FilippovAM <cyberross@rambler.ru>
2018-01-18 02:20:45 +0800

    fix

--- js/app.js
+++ js/app.js
@@ -1,2 +1,45 @@
 $(function () {
+    var $sideBarMenu = $('.header__sidebar-menu');
+
+    checkSideBarMenuPosAndInit();
+
+    $(window).resize(function () {
+        checkSideBarMenuPosAndInit();
+    });
+
+
+    function initSideBarMenuPosForMobile() {
+        var offsetTop = 130;
+        var newOffsetTop = 20;
+
+        $(window).bind('scroll.sideBarMenuScroll', function () {
+            if ($(this).scrollTop() > offsetTop - newOffsetTop) {
+                $sideBarMenu.css({
+                    'position': 'fixed',
+                    'top': newOffsetTop + 'px'
+                });
+            } else {
+                $sideBarMenu.css({
+                    'position': 'absolute',
+                    'top': offsetTop + 'px'
+                });
+            }
+        });
+    }
+
+    function checkSideBarMenuPosAndInit() {
+        if ($sideBarMenu.length) {
+            if (window.matchMedia('(max-width: 991px)').matches) {
+                initSideBarMenuPosForMobile();
+            } else {
+                $sideBarMenu.css({
+                    'position': '',
+                    'top': ''
+                });
+
+                $(window).unbind('.sideBarMenuScroll');
+            }
+        }
+    }
 });
+
